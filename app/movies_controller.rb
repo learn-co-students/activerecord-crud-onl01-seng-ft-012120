53f4629f1b059edc349780985f696244697f08ec
Kyle McKenna <mckennakyler@gmail.com>
2020-02-28 07:42:14 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,6 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   }
   movie = Movie.new(attributes)
   movie.save 
+  movie
 end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
@@ -37,13 +38,11 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first[:title]
-  first 
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last[:title]
-  last 
+  Movie.last
 end
 
 def can_get_size_of_the_database
@@ -51,7 +50,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find(1)[:title]
+  Movie.find_by(id:1)
 end
 
 def can_find_by_multiple_attributes
@@ -59,13 +58,16 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.where(title: "Title", release_date: 2000, director: "Me").flatten[0]
+  # Movie.where(title: "Title", release_date: 2000, director: "Me")
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
+
+# https://stackoverflow.com/questions/11161663/find-vs-find-by-vs-where
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.where().order()
+  Movie.where().order(release_date desc)
 end
 
 def can_be_found_updated_and_saved
