2f41cab03ec7f6ad57cf308c398e4e1da662862b
Kyle McKenna <mckennakyler@gmail.com>
2020-03-01 00:58:26 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,6 @@ def can_be_created_with_a_hash_of_attributes
   }
   movie = Movie.new(attributes)
   movie.save 
-  movie
 end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
@@ -32,13 +31,12 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   
   Movie.create do |m|
     m.title = args[:title]
-    m.release_date = args[:release_date]
-    m.save
+    m.release_date = args[:release_date] 
   end
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first
+  Movie.first 
 end
 
 def can_get_the_last_item_in_the_database
@@ -50,7 +48,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id:1)
+  Movie.find_by(@id)
 end
 
 def can_find_by_multiple_attributes
@@ -58,30 +56,27 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  # Movie.where(title: "Title", release_date: 2000, director: "Me")
-  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
+  Movie.find_by(:title => "Title", :release_date => 2000, :director => "Me")
 end
-
-# https://stackoverflow.com/questions/11161663/find-vs-find-by-vs-where
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.where("release_date > 2002").order(release_date: :desc)
+  Movie.where("release_date > 2002").order(release_date: :desc) 
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  movie = Movie.create(title: "Awesome Flick")
-  movie = Movie.update(title: "Even Awesomer Flick")
-  movie.save
-  movie 
+  Movie.create(title: "Awesome Flick")
+  movie = Movie.find_by(title: "Awesome Flick")
+  movie.update(title: "Even Awesomer")
+  movie.save 
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  
+  movie = Movie.find_by(title: "Wat?")
   movie.update(title: "Wat, huh?")
 end
 
@@ -90,12 +85,14 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.all.each{|movie|
+    movie.update(title: "A Movie")
+  }
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   movie.delete
 end
 
@@ -103,5 +100,5 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  Movie.delete 
+  Movie.delete_all 
 end
